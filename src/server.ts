a2673e2503e1d8e6f4004462d2cea90cb3bdc02d
autoaerial <autoaerial@gmail.com>
2026-06-20 22:15:30 +0800

    Set listening port

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import { config } from './config/env';
 
 const s: any = 's';
 
-app.listen(config.port, () => {
+app.listen(config.port, '0.0.0.0', () => {
   console.log(`🚀 Server running on http://localhost:${config.port} [${config.env}]`);
 });
