884bfd499b45e5ee79050348cdf5db25a0801639
autoaerial <autoaerial@gmail.com>
2026-06-20 22:29:33 +0800

    Add logs to track deployment

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import { config } from './config/env';
 
-const s: any = 's';
+console.log("Starting app...");
 
 app.listen(config.port, '0.0.0.0', () => {
   console.log(`🚀 Server running on http://localhost:${config.port} [${config.env}]`);
