b4b53ea32413122e53449f69ed46996d93552708
autoaerial <autoaerial@gmail.com>
2026-06-21 22:30:12 +0800

    Add new env apibaseUrl

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,7 @@ import app from './app';
 import { config } from './config/env';
 
 app.listen(config.port, '0.0.0.0', () => {
-  console.log(`🚀 Server running on http://localhost:${config.port} [${config.env}]`);
+  console.log(
+    `🚀 Server running on ${config.env === 'development' ? `http://localhost:${config.port}` : `${config.apiBaseUrl}`} [${config.env}]`,
+  );
 });
